a563f7f039eee7880a7f0a26209a393da8eb28e8
S-Stefan-S <31670204+S-Stefan-S@users.noreply.github.com>
2017-11-14 14:25:15 -0800

    Change search_file function
    I changed the search_file function into a pdf file converter that I cp and paste from the Net. However, it only print random portion of the pdf file.

--- pdf_analyzer.py
+++ pdf_analyzer.py
@@ -2,32 +2,85 @@ import os
 from time import sleep
 import subprocess
 import PyPDF2
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
 
 
 # open a file
 # open a file from a website
 
 # open a file from a local directory
-file_name = ""
-lines = []
 
 # search relevant files with the input file name
+
+# location
 os.chdir("/Users/qiyshen")
 print(os.getcwd())
 
-while len(lines) != 2:
-    if len(lines) == 0:
-        file_name = raw_input("Enter the pdf file to proceed: ")
-    else:
-        file_name = raw_input("COPY a FULL file name from the list of results above to proceed: ")
-        #enable user to select options. 
-    find_output = subprocess.check_output("find . -name *" + file_name + "* | xargs grep -v Caches | awk '{print $3}'", shell=True)
-    print(find_output)
-    # count the number of lines from the output
-    lines = find_output.split('\n')
+# file search
+def search_file():
+    file_name = ""
+    lines = []
+    while len(lines) != 1:
+        if len(lines) == 0:
+            file_name = raw_input("Enter the pdf file to proceed: ")
+        else:
+            file_name = raw_input("COPY a FULL file PATH from the list of results above to proceed: ")
+            # enable user to select options. 
+        find_output = subprocess.check_output('find . -iname """*' + file_name + '*""" -print0 | xargs -0 grep -v Caches', shell=True)
+        print(find_output)
+        # count the number of lines from the output
+        lines = find_output.split('\n')
+    return file_name 
+
+# def read_file():
+#     print('test')
+#     local_file_name = file_name
+#     pdfFileObj = open(local_file_name, 'rb')
+#     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+#     print(pdfReader.numPages)
+#     pageObj = pdfReader.getPage(0)
+#     pageText = pageObj.extractText()
+#     print(pageText)
+
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+
+
+# start program
+
+file_name = search_file()
+
+print("Did it work?")
+print(file_name)
+
+print(convert_pdf_to_txt(file_name))
+
     
-        
-pdfFileObj = open(file_name, 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-pageObj = pdfReader.getPage(0)
-pageObj.extractText()
+
